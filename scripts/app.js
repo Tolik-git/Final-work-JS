7e308d23c7d5013ecfa9eb31a8d997798d16375a
Panarin Anatoliy <Tolik_chapaev@mail.ru>
2025-04-09 13:10:29 +0300

    q

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,34 @@
 'use strict';
 
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+/* utils */
+
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitsArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitsArray)) {
+        habbits = habbitsArray
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits))
+}
+
+// /* render */
+// function rerenderMenu(activeHabbitId) {
+//     if (!activeHabbitId) {
+//         return
+//     }
+//     for (const habbit of habbits) {
+
+//     }
+// }
+
+
+/* init */
+(() => {
+    loadData()
+})()
