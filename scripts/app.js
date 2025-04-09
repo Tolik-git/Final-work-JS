a0e4dd8c15c7eddb997858e31f48b052502a3fe8
Panarin Anatoliy <Tolik_chapaev@mail.ru>
2025-04-09 13:37:09 +0300

    1

--- scripts/app.js
+++ scripts/app.js
@@ -4,31 +4,18 @@ let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
 
 /* utils */
-
 function loadData() {
     const habbitsString = localStorage.getItem(HABBIT_KEY);
-    const habbitsArray = JSON.parse(habbitsString);
-    if (Array.isArray(habbitsArray)) {
-        habbits = habbitsArray
+    const habbitArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
     }
 }
 
 function saveData() {
-    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits))
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
-// /* render */
-// function rerenderMenu(activeHabbitId) {
-//     if (!activeHabbitId) {
-//         return
-//     }
-//     for (const habbit of habbits) {
-
-//     }
-// }
-
-
-/* init */
 (() => {
-    loadData()
+    loadData();
 })()
